1fe1873ad833a948f0f7da709617a1308f5c1c15
Abd elrhman Arafa Hassanien <abdoarafa72@yahoo.com>
2019-05-30 14:20:33 +0200

    add main component

--- project Task/project Task/ViewController.swift
+++ project Task/project Task/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var oldPasswordLabel: UITextField!
+    
+    @IBOutlet weak var newPasswordLabel: UITextField!
+    
+    @IBOutlet weak var confirmLabel: UITextField!
+    
+    @IBOutlet weak var updatePasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myColor = UIColor.black
+        oldPasswordLabel.layer.borderColor = myColor.cgColor
+        newPasswordLabel.layer.borderColor = myColor.cgColor
+        confirmLabel.layer.borderColor = myColor.cgColor
+        updatePasswordButton.layer.borderColor = myColor.cgColor
+        
+        oldPasswordLabel.layer.borderWidth = 1.0
+        newPasswordLabel.layer.borderWidth = 1.0
+        confirmLabel.layer.borderWidth = 1.0
+        updatePasswordButton.layer.borderWidth = 1.0
     }
 
     override func didReceiveMemoryWarning() {
