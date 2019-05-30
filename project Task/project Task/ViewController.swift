d958c067f2bda96414dec13fe3eca17bd1f0a64e
Arafa <abdoarafa72@yahoo.com>
2019-05-30 16:11:14 -0700

    reset password

--- project Task/project Task/ViewController.swift
+++ project Task/project Task/ViewController.swift
@@ -21,18 +21,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       initFieldBorder()
+       let passwordImage = UIImage(named: "hideicon")
+        addRightImageTo(txtField: oldPasswordLabel, andImage: passwordImage!)
+        addRightImageTo(txtField: newPasswordLabel, andImage: passwordImage!)
+        addRightImageTo(txtField: confirmLabel, andImage: passwordImage!)
+        
+    }
+    func initFieldBorder()
+    {
         let myColor = UIColor.black
         oldPasswordLabel.layer.borderColor = myColor.cgColor
-        newPasswordLabel.layer.borderColor = myColor.cgColor
-        confirmLabel.layer.borderColor = myColor.cgColor
-        updatePasswordButton.layer.borderColor = myColor.cgColor
-        
         oldPasswordLabel.layer.borderWidth = 1.0
+        
+        newPasswordLabel.layer.borderColor = myColor.cgColor
         newPasswordLabel.layer.borderWidth = 1.0
+        
+        confirmLabel.layer.borderColor = myColor.cgColor
         confirmLabel.layer.borderWidth = 1.0
+        
+        updatePasswordButton.layer.borderColor = myColor.cgColor
         updatePasswordButton.layer.borderWidth = 1.0
     }
-
+    func addRightImageTo(txtField :UITextField , andImage img : UIImage)
+    {
+        let rightImageView = UIImageView(frame: CGRect( x : 0.0 , y: 0.0 , width :img.size.width - 5 , height : img.size.height - 5 ))
+        rightImageView.image = img
+        txtField.rightView = rightImageView
+        txtField.rightViewMode = .always
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
