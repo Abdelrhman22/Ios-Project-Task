40dcbabb32ff77bf809ca4272b83a24c62738680
Arafa <abdoarafa72@yahoo.com>
2019-06-03 06:10:03 -0700

    loadingPageController

---
+++ project Task/project Task/LoadingPageViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoadingPageViewController.swift
+//  project Task
+//
+//  Created by Esraa Hassan on 6/3/19.
+//  Copyright © 2019 Abd elrhman. All rights reserved.
+//
+
+import UIKit
+
+class LoadingPageViewController: UIViewController {
+
+    @IBAction func faceookBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func gmailBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func haveAccountBtn(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func signUpBtn(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
